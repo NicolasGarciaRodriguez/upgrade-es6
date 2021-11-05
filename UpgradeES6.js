25db1fd224ff92a78837cbe14a4b4ced825ff64e
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-11-05 15:11:02 +0100

    Destructuring

--- UpgradeES6.js
+++ UpgradeES6.js
@@ -23,3 +23,51 @@
 // }
 
 // suma(2, 8)
+
+
+
+// 2. Destructuring
+
+// --------------------------2.1------------------------
+
+// const game = { title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020 }
+// let { title, gender, year } = game
+// console.log(title, gender, year)
+
+// --------------------------2.2------------------------
+
+// const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+// let fruit1 = fruits[0]
+// let fruit2 = fruits[1]
+// let fruit3 = fruits[2]
+
+// console.log(fruit1, fruit2, fruit3)
+
+// --------------------------2.3------------------------
+
+// const animalFunction = () => {
+//     return { name: 'Bengal Tiger', race: 'Tiger' }
+// };
+
+// let { name } = animalFunction()
+// let { race } = animalFunction()
+
+// console.log(name)
+// console.log(race)
+
+// --------------------------2.4------------------------
+
+// const car = { name: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+// let { name, itv } = car
+
+// let año1 = itv[0]
+// let año2 = itv[1]
+// let año3 = itv[2]
+
+// console.log(name)
+// console.log(itv)
+// console.log(año1)
+// console.log(año2)
+// console.log(año3)
