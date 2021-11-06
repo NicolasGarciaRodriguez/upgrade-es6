949bb5c10d5006403f5e4e36bd8cab7c2b5e3a59
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-11-06 09:46:25 +0100

    Find

--- UpgradeES6.js
+++ UpgradeES6.js
@@ -255,3 +255,45 @@
 //         }
 //     })
 // }
+
+
+// 6. Find
+
+// --------------------------6.1------------------------
+
+// const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+// const found = numbers.find(num => num === 100)
+// console.log(found)
+
+
+// --------------------------6.2------------------------
+
+// const movies = [
+//     { title: 'Madagascar', stars: 4.5, date: 2015 },
+//     { title: 'Origen', stars: 5, date: 2010 },
+//     { title: 'Your Name', stars: 5, date: 2016 }
+// ];
+// const movieFind = movies.find(movie => movie.date === 2010)
+// console.log(movieFind)
+
+// --------------------------6.3------------------------
+
+// const aliens = [
+//     { name: 'Zalamero', planet: 'Eden', age: 4029 },
+//     { name: 'Paktu', planet: 'Andromeda', age: 32 },
+//     { name: 'Cucushumushu', planet: 'Marte', age: 503021 }
+// ];
+
+// const mutations = [
+//     { name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor' },
+//     { name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna' },
+//     { name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer' }
+// ];
+
+// const alienFind = aliens.find(alien => alien.name === "Cucushumushu")
+// const mutationFind = mutations.find(mutation => mutation.name === "Porompompero")
+
+// const newAlien = {...alienFind, mutation: mutationFind }
+
+// console.log(newAlien)
