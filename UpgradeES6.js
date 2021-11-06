26f7485085a7218194beab9190695aafa8fa594d
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-11-06 10:50:28 +0100

    Bonus

--- UpgradeES6.js
+++ UpgradeES6.js
@@ -364,3 +364,27 @@
 // const sumaNotas = exams.reduce((acc, alum) => acc + alum.score, 0)
 // const mediaNotas = sumaNotas / exams.length
 // console.log(mediaNotas)
+
+
+// 8. Bonus
+
+// --------------------------8.1------------------------
+
+// const videogames = [
+//     { name: 'Final Fantasy VII', genders: ['RPG'], score: 9.5 },
+//     { name: 'Assasins Creed Valhala', genders: ['Aventura', 'RPG'], score: 4.5 },
+//     { name: 'The last of Us 2', genders: ['Acción', 'Aventura'], score: 9.8 },
+//     { name: 'Super Mario Bros', genders: ['Plataforma'], score: 8.5 },
+//     { name: 'Genshin Impact', genders: ['RPG', 'Aventura'], score: 7.5 },
+//     { name: 'Legend of Zelda: Breath of the wild', genders: ['RPG', 'La cosa más puto bonita que he visto nunca'], score: 10 },
+// ]
+
+// const RPGGames = videogames.filter(function(game) {
+//     return game.genders.includes("RPG")
+// })
+
+// const RPGGamesTotalScore = RPGGames.reduce((acc, game) => acc + game.score, 0)
+
+// const RPGGamesMedia = RPGGamesTotalScore / RPGGames.length
+
+// console.log(RPGGamesMedia)
